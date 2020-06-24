7ce3662131f87b3f200790ff340bf6e2e5d54bdb
Githel Lynn Suico <githelsuico@fb.com>
2020-06-24 10:39:35 -0700

    Initial Commit

---
+++ Filmzilla/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Filmzilla
+//
+//  Created by Githel Lynn Suico on 6/24/20.
+//  Copyright © 2020 Githel Lynn Suico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
